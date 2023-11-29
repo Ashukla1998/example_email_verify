a260e1050a83acd8ade517167640f6ed1282eeea
Ashukla1998 <shuklaashu134@gmail.com>
2023-11-29 16:22:49 +0530

    reverification

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#signup'
   get '/verify/:token', to: 'email_verifications#verify', as: :email_verification
   post '/login' , to: 'users#login'
-  get '/resend' , to: 'email_verifications#resend_verification'
+  get '/resend' , to: 'email_verifications#resend_verification', as: :resend
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
